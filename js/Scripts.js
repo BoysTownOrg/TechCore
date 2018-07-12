984f3fd91b885f34379f71f7ff60d6607585f444
Daniel <daniel.faronbi@boystown.org>
2018-07-12 09:35:31 -0500

    added inventory slots and java script console test

---
+++ js/Scripts.js
@@ -0,0 +1,15 @@
+//fill inventory page
+var client_id = "104991348418-70npk7nf3kkbgbsrsmrt4apesoeije5j.apps.googleusercontent.com";
+var api_key = "AIzaSyAYeUbOHiIh9P2xYjRZyM-mztRgynfGLFI";
+
+
+
+window.onclick = show();
+function show(){
+
+
+
+
+  
+  console.log("what's up dudes I like to eat chicken every day");
+}
